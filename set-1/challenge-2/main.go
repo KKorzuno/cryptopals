ae5d6324d8478f0d5926d69822e03f46ec0546e5
Krzysztof <kkorzuno@gmail.com>
2019-08-19 12:24:36 +0200

    XORing hexes done as a function

--- set-1/challenge-2/main.go
+++ set-1/challenge-2/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/base64"
 	"encoding/hex"
 	"fmt"
+	"errors"
 )
 
 func Hex_string_to_base64(msg string) (output string, err error) {
@@ -13,16 +14,14 @@ func Hex_string_to_base64(msg string) (output string, err error) {
 	return
 }
 
-func main() {
-	hex_string := "1c0111001f010100061a024b53535009181c"
-	hex_string2 := "686974207468652062756c6c277320657965"
+func Fixed_XOR_on_strings(hex_string, hex_string2 string) (output_hex_string string, err error) {
 	decoded_hex_string, err := hex.DecodeString(hex_string)
 	decoded_hex_string2, err := hex.DecodeString(hex_string2)
 
 	//fmt.Printf("type: %T", decoded_hex_string)
 	//base64_str, myerr := Hex_string_to_base64(mymsg)
 	if len(decoded_hex_string) != len(decoded_hex_string2) {
-		fmt.Println("error")
+		err = errors.New("Hex strings are not of equal lenght")		
 		return
 	}
 	if err != nil {
@@ -35,7 +34,16 @@ func main() {
 		output[i] = decoded_hex_string[i]^decoded_hex_string2[i]
 	}
 	//fmt.Printf("\n\n\nOUTPUT: %v\n", output)
-	encoded_hex := hex.EncodeToString(output)	
-	fmt.Println(encoded_hex)
+	output_hex_string = hex.EncodeToString(output)	
+	//fmt.Println(encoded_hex)
 	return
+}
+
+func main(){
+	xor_output,myerr := Fixed_XOR_on_strings("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965")
+	if myerr != nil {
+		fmt.Println("error")
+		return
+	}
+	fmt.Println(xor_output)
 }
