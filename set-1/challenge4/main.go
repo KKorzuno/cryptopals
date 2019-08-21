ccbfaee1293a7c51b6d13afd0cd50a1a674128de
Hrvoje <htonkovac@gmail.com>
2019-08-21 12:48:56 +0200

    callenge4

---
+++ set-1/challenge4/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"../challenge2"
+	"encoding/hex"
+	"fmt"
+	"strings"
+	"log"
+	"bufio"
+	"os"
+)
+
+func englishCount(input string) (count int) {
+	input = strings.ToLower(input)
+	for _ , element := range "etaoin shrdlu" {
+		count += strings.Count(input, string(element))
+	}
+	//fmt.Println(input)
+	return
+}
+
+func main() {
+	file, err := os.Open("4.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+		input := scanner.Text()
+		
+		bestLine := maisn(input)
+		// fmt.Println(bestLine)
+		bestLine = bestLine + "hi"
+    } 
+
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+}
+func maisn(input string) (bestString string){
+	
+	texts := make([]string, 255)
+	decodedString := make([]string, 255)
+	// fmt.Println("balalla")
+	for letter := 0; letter < 255; letter++ {
+
+		for i := 0; i < len(input)/len(hex.EncodeToString([]byte(string(letter)))); i++ {
+			// fmt.Printf("\nletter: %v, i:%v\n", letter, i)
+			texts[letter] += hex.EncodeToString([]byte(string(letter)))
+
+		}
+		//fmt.Println(len([]byte(texts[letter])))
+		// fmt.Printf("contents of byte: %v\n",[]byte(texts[letter]))
+		// fmt.Printf("contents of hex(byte):%s\n",texts[letter])
+		// fmt.Println("")
+		
+		msg, err := challenge2.Fixed_XOR_on_hex_strings(input, texts[letter])
+		if err != nil {
+			fmt.Println("PROBLEMS with XOR " + err.Error())
+			return
+		}
+		str2, err2 := hex.DecodeString(msg)
+		if err2 != nil {
+			fmt.Println("PROBLEMS with decoding the outcome hex")
+			return
+		}
+		decodedString[letter] = string(str2)
+		//fmt.Println(decodedString[letter])
+	}
+
+	return findMostEnglishString(decodedString)
+}
+
+func findMostEnglishString(decodedString []string) (bestString string) {
+	var maxCount int
+	for _ , element := range decodedString {
+		currentCount := englishCount(element)
+		if currentCount > maxCount {
+			bestString = element
+			maxCount = currentCount
+		}
+		// fmt.Println(currentCount)
+	}
+	return bestString
+
+}
