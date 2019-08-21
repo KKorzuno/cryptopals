5ad28648de9950e177f08c23fd6c4948737efef9
Krzysztof <kkorzuno@gmail.com>
2019-08-21 11:06:07 +0200

    solved the task, main has a XORer and we added a scoring function

--- set-1/challenge3/main.go
+++ set-1/challenge3/main.go
@@ -4,28 +4,38 @@ import (
 	"../challenge2"
 	"encoding/hex"
 	"fmt"
+	"strings"
 )
+
+func englishCount(input string) (count int) {
+	input = strings.ToLower(input)
+	for _ , element := range "etaoin shrdlu" {
+		count += strings.Count(input, string(element))
+	}
+	//fmt.Println(input)
+	return
+}
+
 
 func main() {
 
 	input := "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-	fmt.Println(len(input))
+	//fmt.Println(len(input))
 	texts := make([]string, 'z'-'a'+1)
-	
-	/*
-	fmt.Println('z' - 'a' + 1)
-	fmt.Println('a')
-	for letter := 0; letter < 'z'-'a'+1; letter++ {
+	decodedString := make([]string, 'z'-'a'+1)
+	//fmt.Println('z' - 'a' + 1)
+	//fmt.Println('a')
+	for letter := 0; letter < 'Z'-'A'+1; letter++ {
 
-		for i := 0; i < len(input); i++ {
-			fmt.Printf("\nletter: %v, i:%v\n", letter, i)
-			texts[letter] += string(letter + 'a')
+		for i := 0; i < len(input)/len(hex.EncodeToString([]byte(string(letter+'A')))); i++ {
+			//fmt.Printf("\nletter: %v, i:%v\n", letter, i)
+			texts[letter] += hex.EncodeToString([]byte(string(letter + 'A')))
 
 		}
-		fmt.Println(hex.EncodeToString([]byte(texts[letter])))
-		fmt.Println("ABC")
-		fmt.Printf("lenght of input: %v length of hex_text: %v", len(input), len(hex.EncodeToString([]byte(texts[letter]))))
-		msg, err := challenge2.Fixed_XOR_on_hex_strings(input, hex.EncodeToString([]byte(texts[letter])))
+		//fmt.Println(len([]byte(texts[letter])))
+		//fmt.Printf("contents of byte: %v\n",[]byte(texts[letter]))
+		//fmt.Printf("contents of hex(byte):%s\n",texts[letter])
+		msg, err := challenge2.Fixed_XOR_on_hex_strings(input, texts[letter])
 		if err != nil {
 			fmt.Println("PROBLEMS with XOR")
 			return
@@ -35,7 +45,19 @@ func main() {
 			fmt.Println("PROBLEMS with decoding the outcome hex")
 			return
 		}
-		fmt.Println(string(str2))
+		decodedString[letter] = string(str2)
+		//fmt.Println(decodedString[letter])
 	}
-	*/
+
+	var maxCount int
+	var bestString string
+	for _ , element := range decodedString {
+		currentCount := englishCount(element)
+		if currentCount > maxCount {
+			bestString = element
+			maxCount = currentCount
+		}
+		fmt.Println(currentCount)
+		fmt.Println(bestString)
+	}
 }
