c51e4687486d46fe53c75a46baf1b22237ae775d
Hrvoje <htonkovac@gmail.com>
2019-09-11 16:00:23 +0200

    Challenge 12 completed.

--- set-2/challenge12/main.go
+++ set-2/challenge12/main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"encoding/base64"
 	"cryptopals/set-2/challenge10"
+	"encoding/base64"
+	"fmt"
 	// "cryptopals/set-2/challenge12"
 	"cryptopals/set-1/challenge8"
-	
-
 )
 
-func main () {
+func main() {
 	unknownStringbase64 := `Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkg
 	aGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBq
 	dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg
@@ -18,52 +16,75 @@ func main () {
 	unknownString, _ := base64.StdEncoding.DecodeString(unknownStringbase64)
 	unknownKey := "YELLOW SUBMARINE"
 
-// encryptionOracle(unknownString,[]byte("asdasd"),unknownKey)
-	keysize := discoverBlocksize(unknownString,unknownKey)
+	keysize := discoverBlocksize(unknownString, unknownKey)
 
-	cyphers := getCypherMap(unknownKey, keysize)
+	//Initialization for the first iteration, generating a keysize long list of A's
+	decryptedPaddedByte := getOneByteShortLetterList(keysize + 1)
+	var exists bool
 
-	
+	encryptedBytes := encryptionOracle([]byte(""), unknownString, unknownKey)
+
+	finalString := ""
+	for j := 0; j < len(encryptedBytes)/keysize; j++ {
+		for i := 0; i < keysize; i++ {
+			// fmt.Println(decryptedPaddedByte, len(decryptedPaddedByte))
+			// fmt.Println
+
+			encryptedBytes := encryptionOracle([]byte(getOneByteShortLetterList(keysize-i)), unknownString, unknownKey)
+			keysizeOfBytes := encryptedBytes[keysize*j:keysize*(j+1)]
+
+			cyphers := getCypherMap(unknownKey, keysize, decryptedPaddedByte[1:])
+
+			decryptedPaddedByte, exists = cyphers[string(keysizeOfBytes)]
+			if exists == false {
+				fmt.Println("FUCCK")
+				// fmt.Println(cyphers)
+			}
+		}
+		// fmt.Println(decryptedPaddedByte)
+		finalString = finalString + decryptedPaddedByte
+	}
+	fmt.Println(finalString)
 }
 
+//AAAAAAAAAAAAAARBF
+func encryptionOracle(input []byte, unknownString []byte, unknownKey string) (encryptedByte []byte) {
+	// fmt.Println(string(append(input,unknownString...)))
+	encryptedByte = challenge10.EncryptEBC(unknownKey, append(input, unknownString...))
 
-func encryptionOracle(unknownString []byte, input []byte, unknownKey string) (encryptedByte []byte) {
-		encryptedByte = challenge10.EncryptEBC(unknownKey,append(input,unknownString...))
-	
-	return 
+	return
 }
 
-func discoverBlocksize (unknownString []byte,unknownKey string) (blockSize int) {
-		listOfAs := ""
-	for i:=1;i<64;i++ {
+func discoverBlocksize(unknownString []byte, unknownKey string) (blockSize int) {
+	listOfAs := ""
+	for i := 1; i < 64; i++ {
 		listOfAs = listOfAs + "AA"
-		minDistance := challenge8.GetMinDistanceInKeysizeMultiComparison(encryptionOracle(unknownString,[]byte(listOfAs),unknownKey), i, 2)
+		minDistance := challenge8.GetMinDistanceInKeysizeMultiComparison(encryptionOracle([]byte(listOfAs), unknownString, unknownKey), i, 2)
 
-		if(minDistance == 0) {
+		if minDistance == 0 {
 			return i
 		}
 	}
 	return 0
 }
 
-func getCypherMap(unknownKey string, keysize int) map[string]string {
+func getCypherMap(unknownKey string, keysize int, oneByteShortLetterList string) map[string]string {
+	cyphers := make(map[string]string, 255)
 
-	bunchOfAs := getOneByteShortLetterList(keysize)
-	cyphers := make(map[string]string,255)
+	for i := 0; i < 255; i++ {
+		val := oneByteShortLetterList + string(i)
+		// key:= string(challenge10.EncryptEBC(unknownKey,[]byte(val)))
+		key := string(encryptionOracle([]byte(""), []byte(val), unknownKey))
 
-	for i:=0;i<255;i++ {
-		val:= bunchOfAs + string(i)
-		key:= string(challenge10.EncryptEBC(unknownKey,[]byte(val)))
 		cyphers[key] = val
-		fmt.Println(key, val)
 	}
 
 	return cyphers
 }
 
-func getOneByteShortLetterList(keysize int) string{
+func getOneByteShortLetterList(keysize int) string {
 	bunchOfAs := ""
-	for i:=0;i<keysize-1;i++ {
+	for i := 0; i < keysize-1; i++ {
 		bunchOfAs = bunchOfAs + "A"
 	}
 	return bunchOfAs
